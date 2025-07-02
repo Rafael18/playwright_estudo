b84a65a4e457d0239489c5eafc223fdf17a5f318
Rafael Batista <rafaeltec_@live.com>
2025-07-02 00:00:34 -0300

    Playwright segundo código, padrão trabalhando com Page Object

--- proejects/zombieplus/tests/example.spec.js
+++ proejects/zombieplus/tests/example.spec.js
@@ -1,65 +1,78 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+const { LandingPage } = require('./pages/LandingPage')
+
+let landingPage
+
+test.beforeEach(async ({ page }) => {
+  landingPage = new LandingPage(page)
+})
+
 test('Deve cadastrar um lead na fila de espera', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-
-  await page.getByRole('button', { name: /Aperte o play/ }).click()
-
-  // await page.locator('#name').fill('rafaeltec_@live.com')
-
-  // await page.locator('input[name=name]').fill('rafaeltec_@live.com')
-
-  // await page.locator('input[placeholder="Seu nome completo"]').fill('rafaeltec_@live.com')
-
-
-  // getByTestId => Deve ser usado quando a tag possui data-testid
-  await expect(
-    page.getByTestId('modal').getByRole('heading')
-  ).toHaveText('Fila de espera')
-
-  // getByPlaceholder => Captura o elemento cujo o placeholder possui o texto informado
-  await page.getByPlaceholder('Seu nome completo').fill('Rafael Batista')
-
-  await page.getByPlaceholder('Seu email principal').fill('rafaeltec_@live.com')
-
-  // await page.getByText('Quero entrar na fila!').click()
-  await page.getByTestId('modal')
-    .getByText('Quero entrar na fila!').click()
-
-  // getByText => Captura elementos que possuam o texto informado (cy.contains)
-  await page.getByText('seus dados conosco').click()
-
-  // page.content() => captura todo o html da página qual teve ação
-  const content = await page.content()
-  console.log(content)
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Rafael Batista', 'rafaeltec_@live.com')
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+  await landingPage.toastHaveText(message)
 
-  await expect(page.locator('.toast')).toHaveText(message)
+  // await page.locator('#name').fill('rafaeltec_@live.com')
+  // await page.locator('input[name=name]').fill('rafaeltec_@live.com')
+  // await page.locator('input[placeholder="Seu nome completo"]').fill('rafaeltec_@live.com')
 
-  await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
+  // getByTestId => Deve ser usado quando a tag possui data-testid
+
+  // getByPlaceholder => Captura o elemento cujo o placeholder possui o texto informado
+
+  // await page.getByText('Quero entrar na fila!').click()
+
+  // getByText => Captura elementos que possuam o texto informado (cy.contains)
+
+  // page.content() => captura todo o html da página qual teve ação
+
+
+
 
   // await page.waitForTimeout(5000)
 });
 
 
-test('Não deve cadastrar com email errado', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+test('Não deve cadastrar com email incorreto', async ({ page }) => {
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Rafael Batista', 'rafaeltec.com')
 
-  await page.getByRole('button', { name: /Aperte o play/ }).click()
+  await landingPage.alertHaveText('Email incorreto')
+  // await page.waitForTimeout(5000)
+});
 
-  await expect(
-    page.getByTestId('modal').getByRole('heading')
-  ).toHaveText('Fila de espera')
+test('Não deve cadastrar quando o nome não é preenchido', async ({ page }) => {
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('', 'rafaeltec_@live.com')
 
-  // getByPlaceholder => Captura o elemento cujo o placeholder possui o texto informado
-  await page.getByPlaceholder('Seu nome completo').fill('Rafael Batista')
-  await page.getByPlaceholder('Seu email principal').fill('rafaeltec.com.br')
+  await landingPage.alertHaveText('Campo obrigatório')
+  // await page.waitForTimeout(5000)
+});
 
-  await page.getByTestId('modal')
-    .getByText('Quero entrar na fila!').click()
+test('Não deve cadastrar quando o campo e-mail não for preenchido', async ({ page }) => {
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Rafael Batista', '')
 
-  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+  await landingPage.alertHaveText('Campo obrigatório')
+  // await page.waitForTimeout(5000)
+});
+
+test('Não deve cadastrar quando nenhum campo é preenchido', async ({ page }) => {
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('', '')
+
+  await landingPage.alertHaveText([
+    'Campo obrigatório',
+    'Campo obrigatório'
+  ])
   // await page.waitForTimeout(5000)
 });
